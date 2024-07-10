da906732f9a1d771667fe699c9da2374396a2460
Damian Flynn <info@damianflynn.com>
2024-07-10 11:05:59 +0100

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -10,7 +10,13 @@ const userConfig: UserConfig = {
             //     page_id: '<page_id>',
             //     target_folder: 'path/relative/to/content/folder'
             // }
+
+            // Get Page ID from URL
+            // https://www.notion.so/About-42464b089a234424a6396c013fa6cef6
+            // https://www.notion.so/<page_name>-<page_id>
+            // <page_id> is the page ID.
             {
+                // About Page
                 page_id: '45eb121158b9489480ec000fd25c812b',
                 target_folder: '.'
             }
@@ -20,8 +26,14 @@ const userConfig: UserConfig = {
             //     database_id: '<database_id>',
             //     target_folder: 'path/relative/to/content/folder'
             // }
+
+            // Get Database ID from URL
+            // https://www.notion.so/4bb8f075358d4efeb575192baa1d62b9?v=3f363ced63f04f54977e2c0c84b482ee
+            // https://www.notion.so/<long_hash_1>?v=<long_hash_2>
+            // <long_hash_1> is the database ID and <long_hash_2> is the view ID.
             {
-                database_id: 'b7b1816c05ec464391c8c111fa242985',
+                // Notion 'Posts' Database
+                database_id: '4bb8f075358d4efeb575192baa1d62b9',
                 target_folder: '.'
             }
         ],
