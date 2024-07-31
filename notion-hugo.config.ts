4866bbd85b6d2c04e7f99c6ad580eb9706ce1c2e
Damian Flynn <info@damianflynn.com>
2024-07-31 11:17:50 +0100

    fix: database update

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,8 +3,8 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://www.notion.so/Hugo-252a519e13ee46c5b576691e1026e7e0',
-        // page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        // page_url: 'https://www.notion.so/Content-Management-252a519e13ee46c5b576691e1026e7e0',
+        page_url: 'https://www.notion.so/Notion-DoIt-b4a946f2fdf94cfebe1a425cd87582da',
         pages: [
             // {
             //     page_id: '<page_id>',
@@ -12,12 +12,13 @@ const userConfig: UserConfig = {
             // }
 
             // Get Page ID from URL
-            // https://www.notion.so/About-42464b089a234424a6396c013fa6cef6
+            // https://www.notion.so/About-Me-42464b089a234424a6396c013fa6cef6
             // https://www.notion.so/<page_name>-<page_id>
             // <page_id> is the page ID.
             {
                 // About Page
-                page_id: '45eb121158b9489480ec000fd25c812b',
+                // page_id: '42464b089a234424a6396c013fa6cef6',
+                page_id: 'f0d707c254654346b1d7c49078ac74a7',
                 target_folder: '.'
             }
         ],
@@ -33,7 +34,8 @@ const userConfig: UserConfig = {
             // <long_hash_1> is the database ID and <long_hash_2> is the view ID.
             {
                 // Notion 'Posts' Database
-                database_id: '4bb8f075358d4efeb575192baa1d62b9',
+                // database_id: '4bb8f075358d4efeb575192baa1d62b9',
+                database_id: '68e960d6f53043819afb6df687714cab',
                 target_folder: '.'
             }
         ],
